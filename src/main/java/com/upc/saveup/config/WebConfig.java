68c8628a59a06750810ef7d243fd40600e0b9dd2
Francisco0903 <red_aldo@hotmail.com>
2023-11-17 22:42:23 -0500

    Segundo commit

--- src/main/java/com/upc/saveup/config/WebConfig.java
+++ src/main/java/com/upc/saveup/config/WebConfig.java
@@ -10,9 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
-                .allowedOrigins("")
+        registry.addMapping("/**")
+                .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowedHeaders(); // GET, POST, PUT, DELETE, PATCH
+                .allowedHeaders("*"); // GET, POST, PUT, DELETE, PATCH
     }
 }
